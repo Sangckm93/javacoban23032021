b9a4f2b9cf6412925e243e3d197580313efcff0f
Nguyen Dinh Sang <sangckm93@gmail.com>
2021-03-25 20:34:56 +0700

    thay đổi file main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,13 @@ public class Main {
         System.out.print("Hello World");
 
         // 1: Tao thu muc .git
+        //      git init
+        // 2: xem sự thay đổi
+        //      git status
+        // 3: Luu trữ sự thay đổi
+        //      git add .
+        // 4: Lưu trữ vào local repo
+        //      git commit -m "tên message"
+        // commit: phat tin hieu
     }
 }
