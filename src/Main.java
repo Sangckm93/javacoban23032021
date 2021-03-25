ce256d3ebf47388eee5d04c3bfafd19e675addcd
Nguyen Dinh Sang <sangckm93@gmail.com>
2021-03-25 21:03:34 +0700

    kiểu dữ liệu

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String [] args) throws Exception{
         System.out.print("Hello World");
-    /*
         // 1: Tao thu muc .git
         //      git init
         // 2: xem sự thay đổi
@@ -14,15 +13,14 @@ public class Main {
         // commit: phat tin hieu
 
         // 1. comment code
-        // ctrl + / , /*
+        // ctrl + / , /* */
+
         // 2. kiểu dữ liệu
         // Nhóm kiểu dữ liệu nguyên thủy: không hỗ trợ phương thức xử lý
-        int , boolean, double, float, char
-
+        //        int , boolean, double, float, char
         // Nhóm kiểu dữ liệu đối tượng: Hỗ trợ các phương thức xử lý
-        String, Integer, Boolean, Double, Float, CharSequence
+        //        String, Integer, Boolean, Double, Float, CharSequence
 
-     */
 
     }
 }
