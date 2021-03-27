e9d0df926040b717ca1a5aa4cbd832e3aacc5ac7
Nguyen Dinh Sang <sangckm93@gmail.com>
2021-03-27 20:08:42 +0700

    cau dieu kien

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String [] args) throws Exception{
-        System.out.print("Hello World");
+//        System.out.print("Hello World");
         /*
          1: Tao thu muc .git
              git init
@@ -32,21 +32,45 @@ public class Main {
             Toán hạng: 1, 2, 3,...
             Toán tử: +, -, *, /
             Biểu thức: phải có giá trị trả về
+            Số : ++ , -- , // , **
 
+            int a = 5;
+            int b = 5;
+            int ketqua = a-- + --b - --b - --a + b-- - --a;
+             5 + --b - --b - --a + b-- - --a; a = 4 , b = 5
+             5 + 4 - --b - --a + b-- - --a; a = 4 , b = 4
+             5 + 4 - 3 - --a + b-- - --a; a = 4 , b = 3
+             5 + 4 - 3 - 3 + b-- - --a; a = 3 , b = 3
+             5 + 4 - 3 - 3 + 3 - --a; a = 3 , b = 2
+             5 + 4 - 3 - 3 + 3 - 2; a = 2 , b = 2
+             9 - 6 + 1
+             4
+
+             System.out.print(ketqua);
+          5. Câu điều kiện (if/else)
+            {} : scope (phạm vi)
+            toán tử 1 ngôi: a+b
+            toán tử 2 ngôi : a++, b++
+            toán tử ba ngôi:
+
+            biểu thức ? giá trị:gia tri;
+            vd: ketqua = (a>b)?1:0;
         */
+        int a = 15;
+        int b = 10;
+        int ketqua = 0;
 
-        int a = 5;
-        int b = 5;
-        int ketqua = a-- + --b + --b - --a + b-- - --a;
-        // 5 + --b + --b - --a + b-- - --a;     a = 4, b = 5;
-        // 5 + 4 + --b - --a + b-- - --a;     a = 4, b = 4;
-        // 5 + 4 + 3 - --a + b-- - --a;     a = 4, b = 3;
-        // 5 + 4 + 3 - 3 + b-- - --a;     a = 3, b = 3;
-        // 5 + 4 + 3 - 3 + 3 - --a;     a = 3, b = 3;
-        // 5 + 4 + 3 - 3 + 2 - 2;     a = 2, b = 2;
-        //
 
-        System.out.print(ketqua);
+        if (a>b) ketqua = 1;
+        else if (a==b) ketqua = 0;
+        else ketqua = -1;
+        System.out.println(ketqua);
+
+//        if (a>b){
+//            System.out.println("A lớn hơn B");
+//        }else{
+//            System.out.println("A bé hơn hoặc bằng B");
+//        }
 
     }
 }
